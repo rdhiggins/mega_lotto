9e3a3ceae42dd891ab2029088289f4f4d80862ac
Rodger Higgins <rdhiggins@gmail.com>
2015-06-03 11:15:52 -0500

    Initial commit

---
+++ lib/mega_lotto.rb
@@ -0,0 +1,13 @@
+require "mega_lotto/version"
+require "mega_lotto/drawing"
+
+begin
+  require "pry"
+rescue LoadError
+end
+
+module MegaLotto
+  # Pry Binding
+  # binding.pry
+
+end
---
+++ spec/mega_lotto_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+module MegaLotto
+  describe VERSION do
+    it 'has a version number' do
+      expect(MegaLotto::VERSION).not_to be nil
+    end
+  end
+
+  describe Drawing do
+    describe "#draw" do
+      let(:draw) { MegaLotto::Drawing.new.draw }
+
+      it "returns an array" do
+        expect(draw).to be_a(Array)
+      end
+       
+      it "returns an array with 5 elements" do
+        expect(draw.size).to eq(5)
+      end
+
+      it "each element is an integer" do
+        draw.each do |drawing|
+          expect(drawing).to be_a(Integer)
+        end
+      end
+
+      it "each element is less than 60" do
+        draw.each do |drawing|
+          expect(drawing).to be < 60
+        end
+      end
+    end
+  end
+end
