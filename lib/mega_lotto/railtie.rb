33c10d7c9d5648c906285a7c9ae1d2fc41b7997b
Rodger Higgins <rdhiggins@gmail.com>
2015-06-05 10:43:46 -0500

    Added in changes through rake integrations

--- lib/mega_lotto/railtie.rb
+++ lib/mega_lotto/railtie.rb
@@ -8,9 +8,15 @@ module MegaLotto
       ActiveSupport.on_load(:action_view) do
         include MegaLotto::Helper
       end
+    end
+    initializer "mega_lotto.action_controller" do
       ActiveSupport.on_load(:action_controller) do
         include MegaLotto::Helper
       end
+    end
+
+    rake_tasks do
+      load "mega_lotto/tasks/mega_lotto.rake"
     end
   end
 end
